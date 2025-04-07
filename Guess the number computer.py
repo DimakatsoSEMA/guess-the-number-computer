647c7d99d3c61ebe85be124b3d1cf9e63f46afcc
Dimakatso Sema <128980864+DimakatsoSEMA@users.noreply.github.com>
2025-04-07 15:55:03 +0200

    Update Guess the number computer.py

--- Guess the number computer.py
+++ Guess the number computer.py
@@ -3,15 +3,19 @@ import random
 
 def guess(x):
     random_number = random.randint(1, x)
-    guess = 0 
+    guess = 0
+    attempts = 0  # Track the number of tries
 
-    while guess != random_number: ##WHILE GUESS IS NOT EQUAL THE NUMBER #While Loop 
-        guess = int(input(f'Guess a number between 1 and {x}:'))
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        attempts += 1  # Increase the count with each guess
+
         if guess < random_number:
-            print('sorry your number is TOO LOW, guess again')
+            print('Too LOW, try again.')
         elif guess > random_number:
-            print('your number is TOO HIGH, guess again')
-        
-    print(f'You did it! the number is {guess}')
+            print('Too HIGH, try again.')
+
+    print(f'You got it! The number was {guess}.')
+    print(f'It took you {attempts} attempts.')
 
 guess(10)
